1af02a2b14b54f7f90c37e2f2cf60594497c587d
alexauren <alexander.auren@dnb.no>
2023-07-07 14:05:33 +0200

    Add syntax error

--- app/frontend/src/index.tsx
+++ app/frontend/src/index.tsx
@@ -26,7 +26,7 @@ export default function App() {
     );
 }
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+ReactDOM.createGRoot(document.getElementById("root") as HTMLElement).render(
     <React.StrictMode>
         <App />
     </React.StrictMode>

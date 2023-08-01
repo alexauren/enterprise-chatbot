5b34c44b100d3cd79fb8288f700dc034097e08c3
hermanhaavik <herrfiks@gmail.com>
2023-08-01 14:33:53 +0200

    implementation of few_shots

--- app/backend/approaches/chatretrievethenread.py
+++ app/backend/approaches/chatretrievethenread.py
@@ -43,6 +43,29 @@ If the question is not in English, translate the question to English before gene
 History:
 {history}
 """
+    few_shot_examples = [
+    {
+        "role": USER, 'content': "Does DNB offer house insurance?",
+        "role": ASSISTANT, 'content': "DNB does offer house insurance [Source Name 1] ",
+    },
+    {
+        "role": USER, 'content': "What is Toppkasko",
+        "role": ASSISTANT, 'content': "Im sorry, i have no sources about that, please ask about something related to house insurance",
+    },
+    {
+        "role": USER, 'content': "Does house insurance also cover fungus and rot?",
+        "role": ASSISTANT, 'content': "House insurance from DNB can also cover fungus and rot as additional add ons, go to the insurance website to find more information about what add ons are availabel and their price [Source Name 2] [Source Name 5]",
+    },
+    {
+        "role": USER, 'content': "What is the best insurance for me?",
+        "role": ASSISTANT, 'content': "There are several factors that are of relevance when finding a suitable insurance, talking with a DNB employee can help you with finding out what fits for you, or checking out the website [Source Name 2]",
+    } ,
+    {
+        "role": USER, 'content': "What is the difference between a cat and a dog",
+        "role": ASSISTANT, 'content': "Im sorry, i have no sources about that, please ask about something related to house insurance",
+    }
+
+    ]
 
     query_prompt_few_shots = [
         {'role' : USER, 'content' : 'What house insurance does DNB provide?' },
@@ -176,7 +199,7 @@ History:
         else:
             prompt = prompt_override.format(follow_up_questions_prompt=follow_up_questions_prompt, sources=sources)
 
-        messages = self.format_chat_messages(system_prompt=prompt, history=history, user_question=history[-1][self.USER])
+        messages = self.format_chat_messages(system_prompt=prompt, history=history, user_question=history[-1][self.USER],few_shot=self.few_shot_examples)
         future = self.executor.submit(self.get_completion, messages, overrides)
         try:
             completion = future.result(timeout=timeout)
@@ -206,7 +229,7 @@ History:
 
         messages.append({"role": self.USER, "content": user_question})
 
-
+        print(messages)
         return messages
 
     def history_as_text(self, history):
